a0215ce4ad59cc4cd23b2a40c9b0e997761bb56e
prjct0192 <project.nemesis92@gmail.com>
2020-10-22 12:32:28 +0300

    first commit

---
+++ src/app/admin/shared/services/auth.service.ts
@@ -0,0 +1,58 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {FbAuthResponse, User} from '../../../shared/interfaces';
+import {Observable, Subject, throwError} from 'rxjs';
+import {environment} from '../../../../environments/environment';
+import {catchError, tap} from 'rxjs/operators';
+
+@Injectable()
+export class AuthService {
+  public error$: Subject<string> = new Subject<string>();
+  constructor(private http: HttpClient) {}
+  get token(): string {
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
+  }
+  login(user: User): Observable<any> {
+    user.returnSecureToken = true;
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
+      );
+  }
+  private handleError(error: HttpErrorResponse): any {
+    const {message} = error.error.error;
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный Email');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный пароль');
+        break;
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Такой Email не найден');
+        break;
+    }
+    return throwError(error);
+  }
+  logout(): any{
+    this.setToken(null);
+  }
+  isAuthenticated(): boolean{
+    return !!this.token;
+  }
+  setToken(response: FbAuthResponse  | null): any{
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAEtttQr_Xjj_IrCyVaw33mo4cmI2rjtJ8',
+  fbDbUrl: 'https://angular-blog-bd991.firebaseio.com'
 };
